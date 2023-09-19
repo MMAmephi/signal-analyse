bc9d90b87c70e9ff17569e77f9604704ce5ce157
MMAmephi <merkulovm12@gmail.com>
2023-09-19 19:59:59 +0300

    updated

--- 1/task1.cpp
+++ 1/task1.cpp
@@ -7,30 +7,27 @@
 
 using namespace std;
 
-int main()
-{
-    double F = 10.; // частота дискретизации
-    int quantization_levels_num = 256; // количество уровней равномерного квантования
-    double quantization_min = 0., quantization_max = 1.;
-    int N_samples = 50;
-    //int digital_signal[6] = {}; // коды квантования цифрового сигнала
-    const char csv_file_name[64] = "data.csv"; // имя файла для вывода
-
-    double phi = 0.; //Начальная фаза
-    double T = 2.; //Период
-    double t=0.5; //Длительность импульса
-    double A=3.; //Амплитуда импульса
-
+int sine_signal(int N_samples, double T, double phi, double F=10.0, int quantization_levels_num=256, double quantization_min = -1., double quantization_max = 1.){
+    const char csv_file_name[64] = "data.csv";
     ofstream csv_file;
     csv_file.open(csv_file_name);
-    csv_file << "time, signal\n"; // записать заголовки колонок
-    /*for (int i = 0; i < N_samples; ++i)
+    csv_file << "time, signal\n";
+    for (int i = 0; i < N_samples; ++i)
     {
-        double signal_val = trunc((sin(2*M_PI*i/F/T + phi)*(quantization_levels_num - 1))/(quantization_max - quantization_min)) * \
+        double signal_val = trunc((sin(2*3.1415*i/F/T + phi)*(quantization_levels_num - 1))/(quantization_max - quantization_min)) * \
             ((quantization_max - quantization_min)/(quantization_levels_num - 1));
         csv_file << (i / F) << "," << signal_val << "\n";
-    }*/
-    /*for (int i = 0; i < N_samples; ++i)
+    }
+    csv_file.close();
+    return 0;
+}
+
+int triangular_signal(int N_samples, double T, double phi, double t, double F=10.0, int quantization_levels_num=256, double quantization_min = 0., double quantization_max = 1.){
+    const char csv_file_name[64] = "data.csv";
+    ofstream csv_file;
+    csv_file.open(csv_file_name);
+    csv_file << "time, signal\n";
+    for (int i = 0; i < N_samples; ++i)
     {
         double signal_val=0;
         double* psv=&signal_val;
@@ -40,10 +37,18 @@ int main()
         else{
             *psv = 0;
         }
-        //cout <<  trunc((-abs(i/F-t/2)+t/2)*(quantization_levels_num - 1)/(quantization_max - quantization_min)) << ", " << signal_val << "\n";
         csv_file << (i / F) << "," << *psv << "\n";
-    }*/
-    /*for (int i = 0; i < N_samples; ++i)
+    }
+    csv_file.close();
+    return 0;
+}
+
+int rectangular_signal(int N_samples, double T, double phi, double t, double A, double F=10.0, int quantization_levels_num=256, double quantization_min = 0., double quantization_max = 1.){
+    const char csv_file_name[64] = "data.csv";
+    ofstream csv_file;
+    csv_file.open(csv_file_name);
+    csv_file << "time, signal\n";
+    for (int i = 0; i < N_samples; ++i)
     {
         double signal_val=0;
         double* psv=&signal_val;
@@ -56,10 +61,18 @@ int main()
         else{
             *psv = 0;
         }
-        //cout <<  trunc((-abs(i/F-t/2)+t/2)*(quantization_levels_num - 1)/(quantization_max - quantization_min)) << ", " << signal_val << "\n";
         csv_file << (i / F) << "," << *psv << "\n";
-    }*/
-    /*double t1_start = (rand()%(N_samples/2-int(t*F)))/F;
+    }
+    csv_file.close();
+    return 0;
+}
+
+int random_signal(int N_samples, double t, double A, double F=10.0, int quantization_levels_num=256, double quantization_min = 0., double quantization_max = 1.){
+    const char csv_file_name[64] = "data.csv";
+    ofstream csv_file;
+    csv_file.open(csv_file_name);
+    csv_file << "time, signal\n";
+    double t1_start = (rand()%(N_samples/2-int(t*F)))/F;
     double t1_end = t1_start + t;
     double t2_start = (rand()%(N_samples-int(t*F))+trunc(t1_end*F))/F;
     double t2_end = t2_start + t;
@@ -74,9 +87,26 @@ int main()
         else{
             *psv = 0;
         }
-        //cout <<  trunc((-abs(i/F-t/2)+t/2)*(quantization_levels_num - 1)/(quantization_max - quantization_min)) << ", " << signal_val << "\n";
         csv_file << (i / F) << "," << *psv << "\n";
-    }*/
+    }
     csv_file.close();
+    return 0;
+}
+
+int main()
+{
+    double F = 10.; // частота дискретизации
+    int quantization_levels_num = 256; // количество уровней равномерного квантования
+    double quantization_min = 0., quantization_max = 1.;
+    int N_samples = 50;
+    //int digital_signal[6] = {}; // коды квантования цифрового сигнала
+    double phi = 0.; //Начальная фаза
+    double T = 2.; //Период
+    double t=0.5; //Длительность импульса
+    double A=3.; //Амплитуда импульса
+    //sine_signal(50, 2.0, 0.5);
+    //triangular_signal(50, 2.0, 0.5, 1.0);
+    //rectangular_signal(50, 2.0, 1.0, 1.0, 3.0);
+    random_signal(100, 1.0, 3.0);
     return 0;
 }
